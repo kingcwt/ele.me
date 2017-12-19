479abca33f225f833fa8493db42c8ef01aaf5f23
isYourDad <zhunanll@foxmail.com>
2017-12-19 11:39:03 +0800

    添加了localstorage插件

--- src/containers/SearchInfo/index.js
+++ src/containers/SearchInfo/index.js
@@ -1,7 +1,12 @@
 import React,{Component} from 'react';
 import SearchHeader from "./components/SearchHeader/index";
 import SearchList from "./components/SearchList/index";
+import  WebStorageCache from 'web-storage-cache'
 export default class SearchInfo extends Component{
+    componentDidMount(){
+        this.wsCache = new WebStorageCache();
+        this.wsCache.set('username', 'wqteam', {exp : 100});
+    }
     render(){
         return (
             <div className="search">
