6714c2793cc38e6539512acb4f9bb4fd1ee48f9f
kingcwt <1845023762@qq.com>
2017-12-22 18:24:57 +0800

    3次提交

--- src/containers/Discover/index.js
+++ src/containers/Discover/index.js
@@ -1,15 +1,10 @@
 import React,{Component} from 'react';
-import './index.less'
 import Header from '../../components/Header'
 export default class Order extends Component{
     render(){
         return (
             <div>
                 <Header title="发现"/>
-                <div className='scroll'>
-                    <img src={require('./1.jpg')} alt=""/>
-                </div>
-
             </div>
         )
     }
--- src/store/action-types.js
+++ src/store/action-types.js
@@ -1,10 +1,1 @@
-
-
-//注册
-export const REG='REG';
-//登陆
-export const LOGIN='LOGIN';
-//修改密码
-export const UPDATEPASSWORD='UPDATEPASSWORD';
-
-
+export const ORDER_LISTS='ORDER_LISTS';
