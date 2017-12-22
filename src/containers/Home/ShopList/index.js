340135da46b09d22a3cb11aca9945510ac30b165
lin930421 <445383331@qq.com>
2017-12-22 17:04:16 +0800

    1.0

--- src/containers/Home/ShopList/index.js
+++ src/containers/Home/ShopList/index.js
@@ -46,7 +46,7 @@ class ShopList extends Component {
                             </svg>
                           </div>
                         </div>
-                        <span className='num'>{item.rating}</span>
+                        <span className='num'>{(item.rating).toFixed(1)}</span>
                         <span className='num'>月售{item.recent_order_num}单</span>
                       </div>
                       {item.delivery_mode?<div className='deliver'>
@@ -72,7 +72,7 @@ class ShopList extends Component {
                     </div>
                     <div className='line'></div>
                     <div className="show_active">
-                      {/*<Active data={item.activities}/>*/}
+                      <Active data={item.activities}/>
                     </div>
                   </div>
                 </div>
